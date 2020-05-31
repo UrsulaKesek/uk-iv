59ebd847ec7b9a58a5c1af47bab5e4a40a6da4d7
Ursula Kesek <ursky@blueyonder.co.uk>
2020-05-31 15:21:22 +0100

    Updates

---
+++ precache-manifest.2cffaa6955507a346b88080e9bfda113.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7725a39317f872c80a7996b7b4f04f85",
+    "url": "/uk-iv/index.html"
+  },
+  {
+    "revision": "23f2ec5ba25d85f87460",
+    "url": "/uk-iv/static/css/main.9ba963d8.chunk.css"
+  },
+  {
+    "revision": "1beea806db24aabf1000",
+    "url": "/uk-iv/static/js/2.0488de64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uk-iv/static/js/2.0488de64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f2ec5ba25d85f87460",
+    "url": "/uk-iv/static/js/main.13c2a667.chunk.js"
+  },
+  {
+    "revision": "2147e048e0c88d7b2b61",
+    "url": "/uk-iv/static/js/runtime-main.8843926f.js"
+  },
+  {
+    "revision": "372e4c21a17ff6536044edd9d08ba89b",
+    "url": "/uk-iv/static/media/bottle.372e4c21.png"
+  },
+  {
+    "revision": "42b22a5601458b74cb3571cda443aeb5",
+    "url": "/uk-iv/static/media/bottlei.42b22a56.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.305a1ece471556e4ae49e45ef7f0f2a9.js"
+  "/uk-iv/precache-manifest.2cffaa6955507a346b88080e9bfda113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uk-iv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
